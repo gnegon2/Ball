0d2950f446de30e65b3b8283c8def92ba76c6d72
Mateusz Grzegorzek <gnegon2@gmail.com>
2015-10-17 18:25:56 +0200

    Scores
    Scores

--- Ball/Assets/Scripts/PlayerController.cs
+++ Ball/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody playerRigidbody;
+    private int count;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         playerRigidbody = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 	
 	// Update is called once per frame
@@ -23,5 +32,22 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
         playerRigidbody.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }      
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 4)
+            winText.text = "You Win!";
     }
 }
