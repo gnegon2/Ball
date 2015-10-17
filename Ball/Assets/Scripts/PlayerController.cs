aa9846cc1b095e317feeaca603849fabd76fa0c5
Mateusz Grzegorzek <gnegon2@gmail.com>
2015-10-17 16:13:00 +0200

    Player and Camera Controller
    Player and Camera Controller added

---
+++ Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody playerRigidbody;
+	// Use this for initialization
+	void Start () {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        playerRigidbody.AddForce(movement * speed);
+    }
+}
